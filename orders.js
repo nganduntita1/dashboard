a239c924a233715a26ef85e3a5d6beac53e34a33
Ngandu Ntita <nganduntita@gmail.com>
2022-04-25 14:21:55 +0200

    Dashboard-Page-README-FOR-LINK
    click the README For Link

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "65789",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone With Camera",
+    productNumber: "74689",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "LARVENDER KF102 Drone",
+    productNumber: "76389",
+    paymentStatus: "Refounded",
+    shipping: "Declined",
+  },
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "98346",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "98844",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "DJI Air 2S",
+    productNumber: "76497",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lozenge Drone",
+    productNumber: "86743",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
